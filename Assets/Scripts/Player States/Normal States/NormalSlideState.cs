ba764ff221505216824da313f0e79141d399ca52
ryan_pc <rytang20@gmail.com>
2024-03-14 23:15:54 +0800

    Added Base State for Sliding

---
+++ Assets/Scripts/Player States/Normal States/NormalSlideState.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class NormalSlideState : BaseState<PlayerController>
+{
+    public override void CaptureInput()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void CheckStateTransition()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void FixedUpdateState()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void InitialiseSubState()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnStateCollisionEnter(Collision2D collision)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnStateCollisionExit(Collision2D collision)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnStateCollisionStay(Collision2D collision)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void UpdateState()
+    {
+        throw new NotImplementedException();
+    }
+}
